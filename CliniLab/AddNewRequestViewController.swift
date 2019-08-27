168ba9c879d6e303b720d0c86daedb0b2b3f66c2
Mahnoor <mahnoorfatimabaloch94@hotmail.com>
2019-08-27 16:14:19 +0500

    first commit

---
+++ CliniLab/AddNewRequestViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddNewRequestViewController.swift
+//  CliniLab
+//
+//  Created by Akif bilal on 3/24/19.
+//  Copyright © 2019 maclogix. All rights reserved.
+//
+
+import UIKit
+
+class AddNewRequestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
